43d36503c1296f4360f31e0563f881d42f862e81
michael moreno <michaelmoreno@MacBook-Pro-7.fios-router.home>
2022-05-19 10:53:50 -0400

    updated pseudo code

---
+++ script.js
@@ -0,0 +1,17 @@
+// Dependencies
+
+// State
+
+// Function
+    // set a timer to display current time (input a moment.js)
+    // add project button should display a popup with forms
+            // project name (text input)
+            // project type (dropdown)
+            // hourly rate (text input)
+            // due date (calendar dropdown)
+            // submit button
+            // close button
+
+// User Interactions
+    // user should click "add project" and the dialog popup should appear
+// Initializationn
